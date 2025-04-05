58fb5b0020ec3d76e3b50bcf228c62a401cdf191
minjujo119 <minjujo119@gmail.com>
2025-04-05 15:01:32 -0400

    헤더 모션 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,92 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { motion } from "motion/react";
+import { useState } from "react";
 
+// style
 const HeaderEl = styled.header`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
+  max-width: 1200px;
   min-height: 60px;
   padding: 24px 0;
+  margin: 0 auto;
+  box-sizing:border-box;
 `;
-const Menu = styled.nav`
-  margin-top: 12px;
-  a {
-    color: ${(props) => props.theme.txtColor};
-    padding: 8px;
+const TabList = styled(motion.nav)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  padding: 4px;
+  border-radius: 12px;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+`;
+const TabItem = styled(motion(Link))`
+  z-index: 10;
+  position: relative;
+  display: block;
+  font-size: 16px;
+  padding: 8px 8px 9px 8px;
+  color: rgba(255, 255, 255, 0.5);
+  &:hover {
+    color: #fff;
   }
+`;
+const SmoothTab = styled(motion.span)`
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.pointColor};
 `;
 const Title = styled.h1`
   font-size: 32px;
   color: ${(props) => props.theme.pointColor};
 `;
 
+// content
+const linkContent = [
+  { path: "/", text: "POPULAR" },
+  { path: "/coming-soon", text: "COMING SOON" },
+  { path: "/now-playing", text: "NOW PLAYING" },
+];
+
 const Header = () => {
+  const [selectedTab, setSelectedTab] = useState<string>(
+    `${linkContent[0].text}`
+  );
+
   return (
     <HeaderEl>
       <Link to={"/"}>
         <Title>MJflix</Title>
       </Link>
-      <Menu>
-        <Link to={"/"}>POPULAR</Link>
-        <Link to={"/coming-soon"}>COMING SOON</Link>
-        <Link to={"/now-playing"}>NOW PLAYING</Link>
-      </Menu>
+      <TabList>
+        {linkContent.map((tab) => {
+          return (
+            <TabItem
+              key={`${tab.text}`}
+              to={`${tab.path}`}
+              onClick={() => setSelectedTab(`${tab.text}`)}
+            >
+              {selectedTab === `${tab.text}` ? (
+                <>
+                  <SmoothTab layoutId="tab" />
+                  <span style={{ color: "#fff" }}>{tab.text}</span>
+                </>
+              ) : (
+                <span>{tab.text}</span>
+              )}
+            </TabItem>
+          );
+        })}
+      </TabList>
     </HeaderEl>
   );
 };
